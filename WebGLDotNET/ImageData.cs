bc58339e7cfbbbab685902317db7b5276b98901d
Kenneth Pouncey <kjpou@pt.lu>
2019-04-02 14:32:38 +0200

    Use new HostObject instead of global and NewJSObject

--- WebGLDotNET/ImageData.cs
+++ WebGLDotNET/ImageData.cs
@@ -1,5 +1,6 @@
 ﻿using WebAssembly;
 using WebAssembly.Core;
+using WebAssembly.Host;
 
 namespace WebGLDotNET
 {
@@ -7,10 +8,11 @@ namespace WebGLDotNET
     {
         public ImageData(byte[] image, int imageWidth, int imageHeight)
         {
+            // Use the following after issue https://github.com/mono/mono/issues/13787 is fixed
+            //var uint8ClampedArray = Uint8ClampedArray.From(image);
             var uint8ClampedArray = new Uint8ClampedArray(image.Length);
             uint8ClampedArray.CopyFrom(image);
-            var imageDataObject = (JSObject)Runtime.GetGlobalObject("ImageData");
-            Handle = Runtime.NewJSObject(imageDataObject, uint8ClampedArray, imageWidth, imageHeight);
+            Handle = new HostObject("ImageData", uint8ClampedArray, imageWidth, imageHeight);
         }
 
         public object Handle { get; }
