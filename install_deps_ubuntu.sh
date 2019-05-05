8247a27923101a9c8c42c2da6aae3f92bcb285a7
jacano <jacanosalado@gmail.com>
2019-05-05 18:28:51 +0200

    More care over the deps scripts

--- install_deps_ubuntu.sh
+++ install_deps_ubuntu.sh
@@ -2,14 +2,11 @@
 
 # ubuntu 18.04
 
-sudo apt-get -y update
-
-sudo apt-get -y install gnupg ca-certificates
-
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/prod.list
 sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 
 sudo apt-get -y update
