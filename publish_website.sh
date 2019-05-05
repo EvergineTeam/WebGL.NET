bb1f91484f71bfdec2b970eca4b027b0d6a1bd92
jacano <jacanosalado@gmail.com>
2019-05-05 14:22:01 +0200

    Warning fix

--- publish_website.sh
+++ publish_website.sh
@@ -3,8 +3,4 @@
 rm -rf website/
 cp -rf Samples/bin/Debug/netstandard2.0/ website/
 
-npm install -g npm@latest
-npm install -g surge@latest
-
 surge $(realpath website)/ https://webglnet.surge.sh
-
