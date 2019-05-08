c30821d1c13019cbdb005f12d0a04b779b2a50d4
jacano <jacanosalado@gmail.com>
2019-05-08 17:50:05 +0200

    Condition for WSL enviroment

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,2 +1,5 @@
-export PATH=$(realpath ./tools/):$PATH
-export MSYS_NO_PATHCONV=1
+windows() { [[ -n "$WINDIR" ]]; }
+if windows; then
+	export PATH=$(realpath ./tools/):$PATH
+	export MSYS_NO_PATHCONV=1
+fi
