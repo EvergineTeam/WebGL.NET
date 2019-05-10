04b553631ea47c2d29897ae5b4aff4219403ba97
jacano <jacanosalado@gmail.com>
2019-05-10 23:03:46 +0200

    Treat warnings as errors added to build.sh Viewport for mobile fixed

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@ source scripts/common.sh
 
 defaultProject=WebGL.NET.sln
 defaultRestore=true
+defaultTreatWarningsAsErrors=true
 
 project=${1:-$defaultProject}
 restore=${2:-$defaultRestore}
+treatWarningsAsErrors=${3:-$defaultTreatWarningsAsErrors}
 commithash=$(git rev-parse HEAD)
 
-msbuild /restore:$restore $project -p:CommitHash=$commithash
+msbuild /restore:$restore $project -p:CommitHash=$commithash -p:TreatWarningsAsErrors=$treatWarningsAsErrors
