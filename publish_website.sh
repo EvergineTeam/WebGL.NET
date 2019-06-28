6fd345a25f3d28fcdb8ddad2a087c5c6bb6ba576
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2019-06-28 14:02:55 +0200

    Tests are now published along with samples

--- publish_website.sh
+++ publish_website.sh
@@ -1,5 +1,7 @@
 rm -rf website/www/
 cp -rf src/Samples/bin/Debug/netstandard2.0/ website/www/
+mkdir website/www/tests
+cp -rf src/Tests/bin/Debug/netstandard2.0/ website/www/tests/
 
 cd website/
 npm install
