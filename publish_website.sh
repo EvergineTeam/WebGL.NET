d61e22815cca9ee2d7a4bccb79e4b71bd7656c50
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2019-07-01 12:08:15 +0200

    Clean up

--- publish_website.sh
+++ publish_website.sh
@@ -1,11 +1,7 @@
-set -v
-
 rm -rf website/www/
 cp -rf src/Samples/bin/Debug/netstandard2.0/ website/www/
 mkdir website/www/tests
 cp -rf src/Tests/bin/Debug/netstandard2.0/. website/www/tests/
-
-ls -R website/
 
 cd website/
 npm install
