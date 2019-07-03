84d4bba48f51f72b08917f124d0b64421e059301
jacano <jacanosalado@gmail.com>
2019-07-03 14:34:44 +0200

    GetActiveUniform regression test added

--- src/Tests/WebGL2Tests.cs
+++ src/Tests/WebGL2Tests.cs
@@ -1,5 +1,6 @@
 ﻿using WebAssembly;
 using WebGLDotNET;
+using Xunit;
 
 namespace Tests
 {
@@ -40,6 +41,36 @@ namespace Tests
             var buffer = gl.CreateBuffer();
 
             gl.BindBufferRange(WebGL2RenderingContextBase.UNIFORM_BUFFER, 0, buffer, 0, 4);
+        }
+
+        public void GetActiveUniformRegressionTest()
+        {
+            var vertexShader = gl.CreateShader(WebGLRenderingContextBase.VERTEX_SHADER);
+            gl.ShaderSource(vertexShader, @"attribute vec3 position;
+attribute vec3 color;
+
+uniform mat4 pMatrix;
+uniform mat4 vMatrix;
+uniform mat4 wMatrix;
+
+varying vec3 vColor;
+
+void main(void) {
+    gl_Position = pMatrix * vMatrix * wMatrix * vec4(position, 1.0);
+    vColor = color;
+}");
+            gl.CompileShader(vertexShader);
+            var fragmentShader = gl.CreateShader(WebGLRenderingContextBase.FRAGMENT_SHADER);
+            gl.ShaderSource(fragmentShader, "void main() {}");
+            gl.CompileShader(fragmentShader);
+            var program = gl.CreateProgram();
+            gl.AttachShader(program, vertexShader);
+            gl.AttachShader(program, fragmentShader);
+            gl.LinkProgram(program);
+
+            var uniform = gl.GetActiveUniform(program, 0);
+
+            Assert.IsType(typeof(uint), uniform.Type);
         }
     }
 }
