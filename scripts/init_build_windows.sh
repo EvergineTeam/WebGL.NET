e105d331bebb0ba684a5e355f5686cb5cd6e89f9
jacano <jacanosalado@gmail.com>
2019-07-05 01:29:18 +0200

    Msbuild added to path in windows

--- scripts/init_build_windows.sh
+++ scripts/init_build_windows.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env bash
 
-export MSYS_NO_PATHCONV=1
-
 if [ ! -x "$(command -v msbuild)" ] ; then
 
-	VSCOMMUNITY2017='/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/MSBuild/15.0/Bin/'
-	VSCOMMUNITY2019='/c/Program Files (x86)/Microsoft Visual Studio/2019/Community/MSBuild/Current/Bin/'
+	VSWHEREPATH=$(echo $(cygpath -F 42))"/Microsoft Visual Studio/Installer/vswhere.exe"
+	if [ ! -f "$VSWHEREPATH" ]; then
+		echo "Visual Studio 2017 version 15.2 or greater required"
+		exit 1
+	fi
 
-	VSENTERPRISE2017='/c/Program Files (x86)/Microsoft Visual Studio/2017/Enterprise/MSBuild/15.0/Bin/'
-	VSENTERPRISE2019='/c/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/MSBuild/Current/Bin/'
-
-	export PATH=$VSCOMMUNITY2017:$PATH
-	export PATH=$VSCOMMUNITY2019:$PATH
-
-	export PATH=$VSENTERPRISE2017:$PATH
-	export PATH=$VSENTERPRISE2019:$PATH
-
-	alias msbuild=msbuild.exe
+	MSBUILDWINPATH=$("$VSWHEREPATH" -latest -requires Microsoft.Component.MSBuild -find MSBuild/**/Bin/MSBuild.exe)
+	MSBUILDWINPATH=${MSBUILDWINPATH%"MSBuild.exe"}
+	MSBUILDPATH=$(echo $(cygpath -u $MSBUILDWINPATH))
+	
+	export PATH=$MSBUILDPATH:$PATH
+	alias msbuild=MSBuild.exe
 fi
+
 
 
 
