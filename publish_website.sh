0fa7de9359c4ea6645c4299347773d8de6593955
Marcos Cobeña Morián <marcoscm@me.com>
2019-09-11 11:39:36 +0200

    Fixed Surge publishing —it was pointing to the Debug path :-S

--- publish_website.sh
+++ publish_website.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 rm -rf website/www/
-cp -rf src/Samples/bin/Debug/netstandard2.0/ website/www/
+cp -rf src/Samples/bin/Release/netstandard2.0/ website/www/
 mkdir website/www/tests
-cp -rf src/Tests/bin/Debug/netstandard2.0/. website/www/tests/
+cp -rf src/Tests/bin/Release/netstandard2.0/. website/www/tests/
 
 cd website/
 npm install
